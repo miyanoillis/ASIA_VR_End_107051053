2593897c3da6931819eeaec84b985f9ece3fcc9d
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2021-01-01 22:16:23 +0800

    1.5

--- ASIA_VR_End_107051053/Assets/script/enmy.cs
+++ ASIA_VR_End_107051053/Assets/script/enmy.cs
@@ -1,6 +1,7 @@
-﻿
+﻿using UnityEngine.UI;
 using UnityEngine.AI;
 using UnityEngine;
+using System.Collections;
 
 public class enmy : MonoBehaviour
 {
@@ -10,10 +11,19 @@ public class enmy : MonoBehaviour
     public float cd = 2f;
     private float timer;
 
-    [Header("speed") ,Range(0f,50f)]
+    [Header("speed"), Range(0f, 50f)]
     public float spd = 3;
     [Header("distance"), Range(0f, 50f)]
     public float spdstop = 1;
+    [Header("die sc"), Range(0f, 5f)]
+    public float sc = 0.2f;
+    [Header("txet")]
+    public Text text;
+    [Header("hp")]
+    public int hp = 50;
+    [Header("hit")]
+    public int hit = 10;
+
 
 
     private void Awake()
@@ -28,13 +38,16 @@ public class enmy : MonoBehaviour
 
     public void Update()
     {
+        StartCoroutine(diee());
         track();
         attack();
     }
 
     public void attack()
     {
-        if(nav.remainingDistance < spdstop)
+        if(hp > 0)
+        {
+ if(nav.remainingDistance < spdstop)
         {
             timer += Time.deltaTime;
 
@@ -48,11 +61,42 @@ public class enmy : MonoBehaviour
                 timer = 0;
             }
         }
+        }
+       
     }
 
     public void track()
     {
         nav.SetDestination(player.position);
         ani.SetBool("walk" ,nav.remainingDistance> spdstop);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(hp>0)
+        {
+        if (other.tag == "atk")
+        {
+            addi();
+        }
+        }
+
+    }
+
+    private void addi()
+    {
+        hp -= hit;
+        text.text = ""+hp;
+    }
+
+    private IEnumerator diee()
+    {
+        if(hp <= 0 )
+        {
+            ani.SetBool("die" , true);
+
+            yield return new WaitForSeconds(sc);
+            gameObject.SetActive(false);
+        }
     }
 }
